cd0d4b928ba3ad570d0bb4afc2aa5dbf6d074948
CapitaineToinon <antoinesutter@gmail.com>
2023-03-30 12:44:27 +0200

    Import from prisma instead

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,43 @@
 import { useSignOut } from '@/routes/sign-out'
 import { component$ } from '@builder.io/qwik'
-import { Form, Link, type z } from '@builder.io/qwik-city'
-import type { user } from '@zod'
+import { Form, Link } from '@builder.io/qwik-city'
+import type { User } from '@prisma/client'
 
-export const Navbar = component$<{ user: z.infer<typeof user> | null }>(
-	(props) => {
-		const action = useSignOut()
+export const Navbar = component$<{ user: User | null }>((props) => {
+	const action = useSignOut()
 
-		return (
-			<nav class="border-b">
-				<ul class="flex gap-3 py-3">
-					<li>
-						<Link href="/">Home</Link>
-					</li>
-					{!!props.user && (
-						<>
-							<li>
-								<Link href="/todos">Todos</Link>
-							</li>
-							<li>
-								<Form
-									action={action}
-									spaReset
-								>
-									<button type="submit">Sign out</button>
-								</Form>
-							</li>
-						</>
-					)}
-					{!props.user && (
-						<>
-							<li>
-								<Link href="/sign-up">Sign up</Link>
-							</li>
-							<li>
-								<Link href="/sign-in">Sign in</Link>
-							</li>
-						</>
-					)}
-				</ul>
-			</nav>
-		)
-	}
-)
+	return (
+		<nav class="border-b">
+			<ul class="flex gap-3 py-3">
+				<li>
+					<Link href="/">Home</Link>
+				</li>
+				{!!props.user && (
+					<>
+						<li>
+							<Link href="/todos">Todos</Link>
+						</li>
+						<li>
+							<Form
+								action={action}
+								spaReset
+							>
+								<button type="submit">Sign out</button>
+							</Form>
+						</li>
+					</>
+				)}
+				{!props.user && (
+					<>
+						<li>
+							<Link href="/sign-up">Sign up</Link>
+						</li>
+						<li>
+							<Link href="/sign-in">Sign in</Link>
+						</li>
+					</>
+				)}
+			</ul>
+		</nav>
+	)
+})
